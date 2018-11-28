c3c59909e01169c643e076ba9558733dc605270b
Alex Oliveira <aleques@gmail.com>
2018-11-27 23:46:58 -0200

    brussels

---
+++ others/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    
+    <div style="margin-left: 100px">
+      <h2>
+        {{counter}}
+      </h2>
+    
+      <div>
+        <button (click)="incrementCounter()" (mouseout)="resetCounter()" class="style-button">Click</button>
+      </div>
+    </div>
+
+    `,
+  styles: [`
+  .style-button {
+    display: inline-block;
+    border-radius: 4px;
+    background-color: #f4511e;
+    border: none;
+    color: #FFFFFF;
+    text-align: center;
+    font-size: 28px;
+    padding: 20px;
+    width: 200px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+  }
+  `]
+})
+export class AppComponent implements OnInit {
+  
+  private counter : number;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.resetCounter();
+  }
+
+  public resetCounter() {
+    this.counter = 42;
+  }
+
+  incrementCounter() {
+    this.counter++;
+  }
+
+}
